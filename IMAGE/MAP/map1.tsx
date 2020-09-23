77092a32417fbfa4c8b3b7696f346250af0c787e
ymd9929 <hj-g19033@sist.ac.jp>
2020-09-23 10:00:50 +0900

    マップ作成

---
+++ IMAGE/MAP/map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="map1" tilewidth="32" tileheight="32" tilecount="1312" columns="32">
+ <image source="mapchip1 .png" width="1024" height="1312"/>
+</tileset>
